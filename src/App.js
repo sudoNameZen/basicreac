bd2de511edf2ca9b2e84435c8d4c0e8d00e6d8a8
sudoNameZen <imdewangannitin56@gmail.com>
2023-12-13 16:22:01 +0530

    here basic react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Cake from './Cake';
+function App() {
+  return (
+    
+    <>
+    <h1> this is basic app in react </h1>
+    <Cake/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Cake.js
@@ -0,0 +1,8 @@
+
+function cake(){
+    return(
+        <h1>this is component cake!!</h1>
+    )
+}
+
+export default cake;
